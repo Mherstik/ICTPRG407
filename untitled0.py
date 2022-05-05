48391c7081597e5f2eb0113bc5a829d7caf7313d
Marcus Herstik <marcus@herstik.com>
2022-05-06 08:27:55 +1000

    Create and commit

---
+++ untitled0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 08:21:21 2022
+
+@author: Admin
+"""
+
+name = "Tom"
+
+### Marcus will teach conditional statements
+if name == "Marcus":
+    print("Yay")
+else:
+    print("Boo!!")
