d0f48bf92a0561b94ca22b8aaecc172cfd2d07f3
Marcus Herstik <marcus@herstik.com>
2022-05-06 08:57:51 +1000

    testing age

--- untitled0.py
+++ untitled0.py
@@ -2,13 +2,37 @@
 """
 Created on Fri May  6 08:21:21 2022
 
-@author: Admin
+@author: Marcus Herstik
 """
 
-name = "Tom"
 
-### Marcus will teach conditional statements
-if name == "Marcus":
-    print("Yay")
-else:
-    print("Boo!!")
+age = int(input("what is your age: "))
+
+if age < 18:
+    print("You receive a 20% discount")
+    
+print("Please follow the waiter")
+
+
+# =============================================================================
+# # name = "Tom"
+# name = input("What is your name? ")
+# 
+# ### Marcus will teach conditional statements
+# if name != "Marcus":
+#     print("Yay")
+#     print("I'm not a fan of Marcus")
+#     print("He is smelly!!")
+# else:
+#     print("Boo!!")
+#     
+# number = 2+2
+# print(number)   
+# 
+# decimal = 4.0+2.0
+# print(decimal)
+# 
+# print(type(name))
+# print(type(number))
+# print(type(decimal))
+# =============================================================================
