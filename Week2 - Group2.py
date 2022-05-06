554e39f1a521568de9a607cd4d8ebb6e6b384ca4
Marcus Herstik <marcus@herstik.com>
2022-05-06 14:22:07 +1000

    GradingResult

--- Week2 - Group2.py
+++ Week2 - Group2.py
@@ -5,17 +5,54 @@ Created on Fri May  6 13:00:54 2022
 @author: Marcus Herstik
 """
 
+## Get mark from student and say grade
 
-print("Welcome to the Python Restaurant")
-name = input("What is your name? ")
-print("Nice to meet you", name)
+mark = int(input("What is your grade? "))
+           
+if mark < 50:
+    print("Fail")
+elif mark < 65:
+    print("Pass")
+elif mark < 75:
+    print("Credit")
+elif mark < 85:
+    print("DN")
+else:
+    print ("HD")
 
-age = int(input("what is your age? "))
 
-if age < 18:
-    print("You get 20% off")
-    
-print("Please follow the waiter")
+
+# =============================================================================
+# name = input("What is your name? ")
+# 
+# if not name == "Marcus":
+#     print("You are my favourite")
+# else:
+#     print("Go away!!")
+# 
+# =============================================================================
+
+# =============================================================================
+# print("Welcome to the Python Restaurant")
+# name = input("What is your name? ")
+# print("Nice to meet you", name)
+# 
+# age = int(input("what is your age? "))
+# 
+# if age < 18:
+#     print("You get 20% off")
+#     print("Try the ice-cream")
+# elif age > 45 and age < 60:
+#     print("Would you like a drink?")
+# elif age > 60 and age < 100:
+#     print("You get a 10% discount")
+#     print('Do you need your food mushed up?')
+# elif age == 100:
+#     print("Get a letter from the Queen")
+# else:
+#     print('You are in between')
+# print("Please follow the waiter")
+# =============================================================================
 
 # =============================================================================
 # year = 2022 
