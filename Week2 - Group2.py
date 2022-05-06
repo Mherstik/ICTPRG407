ddbff361768c57494ba89b6ebbb6f11a79919feb
Marcus Herstik <marcus@herstik.com>
2022-05-06 13:31:26 +1000

    Age

--- Week2 - Group2.py
+++ Week2 - Group2.py
@@ -6,11 +6,27 @@ Created on Fri May  6 13:00:54 2022
 """
 
 
-### Revision
-a = int(input("Give me an integer: "))
-b = input("Give me your name: ")
-c = float(input("Give me a decimal: "))
+year = 2022 
+myAge = input("what year where you born")
+currentAge = year - myAge
+print("I am " + str(currentAge) + " this year")
 
-print("a is of type", type(a))
-print("b is of type", type(b))
-print("c is of type", type(c))
+
+# =============================================================================
+# ### Revision
+# a = int(input("Give me an integer: "))
+# b = input("Give me your name: ")
+# c = float(input("Give me a decimal: "))
+# 
+# print(a,b,c)
+# 
+# print(a + c)
+# 
+# print(str(a) + b + str(c))
+# 
+# print("a is of type", type(a))
+# print("b is of type", type(b))
+# print("c is of type", type(c))
+# 
+# print(2+2)
+# =============================================================================
