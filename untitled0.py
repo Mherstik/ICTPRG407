d37d028e0b32710eae3e09487f75d887350ff85a
Marcus Herstik <marcus@herstik.com>
2022-05-06 11:13:15 +1000

    bubble sort pseudo

--- untitled0.py
+++ untitled0.py
@@ -7,6 +7,47 @@ Created on Fri May  6 08:21:21 2022
 
 
 
+# get hours worked
+# gross = hours worked * hourly wage
+# if commissions new gross = gross + comission
+# remove tax = net
+# remove super + salary sac etc = net
+# put net in bank account
+
+'''
+Repeat until no swapping was done {
+    For each position in the list from lowest up to (not including) highest {
+            If the element at this position is bigger than the next position {
+                Swap this element and the next element
+                Record that a swap was done
+                }
+            }
+    }
+
+
+'''
+
+# =============================================================================
+# # a = int(input("Enter 1st integer: "))
+# # b = int(input("Enter 2nd integer: "))
+# # c = int(input("Enter 3rd integer: "))
+# # d = float(input("Enter 4th decimal: "))
+# 
+# # print("Convert d to integer", int(d))
+# # print(a+b+c)
+# # print(a+int(d))
+# 
+# # e = float(input("Enter 5th decimal: "))
+# # f = float(input("Enter 6th decimal: "))
+# # print(d*e/f)
+# 
+# # r = float(input("Radius: "))
+# # print("Radius: ", r, "Diameter: ", r*2, "Area: ", 3.14159*r**2)
+# # print("#####\r\n OPTION 2\r\n ####")
+# # print("Radius: %.1f Diameter: %.1f Area: %.5f" %(r,r*2, 3.14159*r**2))
+# =============================================================================
+
+
 
 ### BREAK 
 
@@ -82,6 +123,8 @@ Created on Fri May  6 08:21:21 2022
 # 
 # decimal = 4.0+2.0
 # print(decimal)
+# 
+# print(decimal + number)
 # 
 # print(type(name))
 # print(type(number))
