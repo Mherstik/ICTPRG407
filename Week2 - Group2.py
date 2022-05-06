ca4b7bc6766fb187cb2e52ed218ef6175465073c
Marcus Herstik <marcus@herstik.com>
2022-05-06 13:18:07 +1000

    First Commit

--- Week2 - Group2.py
+++ Week2 - Group2.py
@@ -2,6 +2,15 @@
 """
 Created on Fri May  6 13:00:54 2022
 
-@author: Admin
+@author: Marcus Herstik
 """
 
+
+### Revision
+a = int(input("Give me an integer: "))
+b = input("Give me your name: ")
+c = float(input("Give me a decimal: "))
+
+print("a is of type", type(a))
+print("b is of type", type(b))
+print("c is of type", type(c))
