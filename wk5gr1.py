b03fcd3c15d4d4e0cb98b0246c762272afb4c62e
Marcus Herstik <marcus@herstik.com>
2022-05-20 08:46:24 +1000

    Create wk5gr1.py

---
+++ wk5gr1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 08:18:26 2022
+
+@author: Admin
+"""
+
+i = 0
+a = []
+tmp = 0
+number_guesses = int(input("How many guesses do you want: "))
+
+while i < number_guesses:
+    i = i + 1 
+    tmp = input("Give me a number: ")
+    
+
+
+# =============================================================================
+# a = int(input("What is a number: "))
+# 
+# b = int(input("Give me a second number: "))
+# 
+# c = input("What do you want to do?\n\r Multiply is '*' \r\n Divide is '\\'\r\n Addition is '+'\r\n Subtract is '-'\r\n ")
+# 
+# ## I want to get 2 numbers and then do a math thing with them
+# 
+# if c == '*':
+#     print(a, "multiplies by ", b, "is", a*b)
+# elif c == '\\':
+#     print(a, "divided by ", b, "is", a/b)
+#     ## Check to see if the number can be divided without a remainder
+#     if ((a/b)%2) == 0:
+#         print("You have a perfect division")
+#     else:
+#         print("You have a remainder of ", a//b)
+# elif c == '+':
+#     print(a, "plus ", b, "is", a+b)
+# elif c == '+':
+#     print(a, "plus ", b, "is", a+b)
+# else:
+#     print("You didn't choose a correct factor")
+# 
+# =============================================================================
+
+
+
+    
