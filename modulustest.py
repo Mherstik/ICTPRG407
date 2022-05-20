f235463801a6be073528dd5c750db356fc342fb7
Marcus Herstik <marcus@herstik.com>
2022-05-20 13:59:48 +1000

    Update

--- modulustest.py
+++ modulustest.py
@@ -8,8 +8,9 @@ Created on Fri May 20 13:41:35 2022
 a = int(input("Give me a number: "))
 b = int(input("Give me another number: "))
 
-## modulus?? %
-print(a%b)
-
 ## floor division?? //
-print(a//b)
+print(b, "goes into",a, a//b, "times")
+
+## modulus?? %
+print("remainder is ", a%b)
+
--- wk5Grp2.py
+++ wk5Grp2.py
@@ -5,14 +5,29 @@ Created on Fri May 20 13:01:26 2022
 @author: Marcus Herstik
 """
 
-numbers = []
 i = 1
+a = 10
+
+## only go to a max of 5
+while int(a) > 5:
+    a = int(input("How many loops do you want: "))
+    print("You must choose a number no higher than 5")
+
+servers = ['A', 'B', "C", 'D', "E"]
+
+for server in servers:
+    while i < a:
+        i = i + 1
+        print("Server "+server)
+        ## print only that amount
+    
 
 #a = 'string'
 #print(a)
-
-b = int(input("How many numbers do you want to give: "))
-a = int(input("give me another number: "))
+# numbers = []
+# i = 1
+# a = int(input("How many numbers do you want to give: "))
+# b = int(input("give me another number: "))
 #print("We will loop "+ str(b)+ " number of times")
 
 ## get a list of numbers
@@ -26,22 +41,27 @@ a = int(input("give me another number: "))
 #     i = i + 1
  
 
-factor = input("What do you want to do? \r\n Multiply is '*' \r\n Divide is '\\' \r\n Addition is '+' \r\n Subtract is '-'\r\n \r\n : " )
-
-# get the numbers and then do factor
-
-if factor == '*':
-    print(a, "multiplied by ", b , "is", a*b)
-elif factor == '\\':
-    print(a, "divided by ", b , "is", a/b)
-    ## Check to see if the number can be divided without a remainder
-    
-elif factor == '+':
-    print(a, "plus", b , "is", a+b)
-elif factor == '-':
-    print(a, "minus", b , "is", a-b)
-else:
-    print("You didn't choose a correct factor!")
+# =============================================================================
+# factor = input("What do you want to do? \r\n Multiply is '*' \r\n Divide is '\\' \r\n Addition is '+' \r\n Subtract is '-'\r\n \r\n : " )
+# 
+# # get the numbers and then do factor
+# 
+# if factor == '*':
+#     print(a, "multiplied by ", b , "is", a*b)
+# elif factor == '\\':
+#     print(a, "divided by ", b , "is", a/b)
+#     ## Check to see if the number can be divided without a remainder
+#     if ((a/b)%2) == 0:
+#         print("You have perfect division")
+#     else:
+#         print("You have a remainder of", a%b)
+# elif factor == '+':
+#     print(a, "plus", b , "is", a+b)
+# elif factor == '-':
+#     print(a, "minus", b , "is", a-b)
+# else:
+#     print("You didn't choose a correct factor!")
+# =============================================================================
     
     
     
