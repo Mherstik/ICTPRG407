73c15589f5f6e144b69d56ee23d4630754a8014e
Marcus Herstik <marcus@herstik.com>
2022-05-20 13:44:13 +1000

    Create modulustest.py

---
+++ modulustest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 13:41:35 2022
+
+@author: Admin
+"""
+
+a = int(input("Give me a number: "))
+b = int(input("Give me another number: "))
+
+## modulus?? %
+print(a%b)
+
+## floor division?? //
+print(a//b)
