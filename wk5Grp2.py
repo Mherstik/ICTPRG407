d0a83fe95195b1a8a642224138cc13caff1cca3c
Marcus Herstik <marcus@herstik.com>
2022-05-20 16:49:26 +1000

    Update wk5Grp2.py

--- wk5Grp2.py
+++ wk5Grp2.py
@@ -11,15 +11,27 @@ while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+
+# convert guess to 0 or 1
+if guess == 'heads':
+    guess = 1
+else:
+    guess = 0
+    
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
+    guess = input()
+    if guess == 'heads':
+        guess = 1
+    else:
+        guess = 0
     if toss == guess:
         print('You got it!')
     else:
         print('Nope. You are really bad at this game.')
+    
 
 
 # # ## count to 4
