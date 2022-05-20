d4293c4682745d3daea966a9a07dc2d19f00103c
Marcus Herstik <marcus@herstik.com>
2022-05-20 13:22:11 +1000

    Update wk5Grp2.py

--- wk5Grp2.py
+++ wk5Grp2.py
@@ -5,3 +5,19 @@ Created on Fri May 20 13:01:26 2022
 @author: Marcus Herstik
 """
 
+numbers = []
+i = 1
+
+a = 'string'
+print(a)
+
+b = int(input("How many numbers do you want to give: "))
+print("We will loop "+ str(b)+ " number of times")
+
+## get a list of numbers
+## loop b amount of times getting numbers in a list
+while i < b:
+    i = i + 2
+    tmp = input("Give me a number: ")
+    numbers.append(tmp)
+    print(i,"\r\n",tmp,"\r\n",numbers)
