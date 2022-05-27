b5d50ea108dae57636f8deea2148831bd175ba8c
Marcus Herstik <marcus@herstik.com>
2022-05-27 15:30:52 +1000

    Create Wk6-Grp2.py

---
+++ Wk6-Grp2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 13:17:59 2022
+
+@author: Marcus
+"""
+
+
+lowPage=1
+highPage=1000
+midPage=0
+target=650
+numLoops=0
+#Stop this search when we're within two pages of our target
+While ((highPage - lowPage) > 2)
+print("Now checking from page " + lowPage + " to page " + highPage)
+midPage = (lowPage + highPage) / 2 #find the middle of this section
+if midPage < target
+lowPage = midPage +1 #our guess was too low, update our lower bound
+else if midpage < target
+highPage = midPage -1 #our guess was too high, update our upper bound
+else
+print "We found the exact page"
+increment numLoops
+EndWhile
+print "We finished between page " + lowPage + " and page " + highPage
+print "We found the right page after " + numLoops + " attempts.
