d9e22bc7d2b4c536206e58a88726ef4cedf64f89
Marcus Herstik <marcus@herstik.com>
2022-05-27 15:48:36 +1000

    Update Wk6-Grp2.py

--- Wk6-Grp2.py
+++ Wk6-Grp2.py
@@ -13,8 +13,8 @@ target=650
 numLoops=0
 #Stop this search when we're within two pages of our target
 while ((highPage - lowPage) > 2):
-    print("Now checking from page " + lowPage + " to page " + highPage)
-    midPage = (lowPage + highPage) / 2 #find the middle of this section
+    print("Now checking from page " + str(lowPage) + " to page " + str(highPage))
+    midPage = (lowPage + highPage) // 2 #find the middle of this section
     if midPage < target:
         lowPage = midPage +1 #our guess was too low, update our lower bound
     elif midPage > target:
@@ -23,7 +23,7 @@ while ((highPage - lowPage) > 2):
         print("We found the exact page")
     numLoops = numLoops+1 
 #    EndWhile
-print("We finished between page " + lowPage + " and page " + highPage)
-print("We found the right page after " + numLoops + " attempts.")
+print("We finished between page " + str(lowPage) + " and page" , highPage)
+print("We found the right page after " + str(numLoops) + " attempts.")
       
       
