649347562d0fa5b78795bb2eda5bcd471ad2719e
Marcus Herstik <marcus@herstik.com>
2022-06-03 08:31:16 +1000

    Update Wk6-Strings-Grp1.py

--- Wk6-Strings-Grp1.py
+++ Wk6-Strings-Grp1.py
@@ -5,3 +5,12 @@ Created on Fri Jun  3 08:27:52 2022
 @author: Marcus
 """
 
+a = 1
+b = 1.0
+c = "Tom"
+d = ["a","b", 1.0, 4]
+
+print("A is a", type(a))
+print("B is a", type(b))
+print("C is a", type(c))
+print("D is a", type(d))
