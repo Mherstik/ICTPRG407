d8a079c827d5dbb11f8a062eb32619a4c38d7758
Marcus Herstik <marcus@herstik.com>
2022-06-03 08:49:18 +1000

    Update Wk6-Strings-Grp1.py

--- Wk6-Strings-Grp1.py
+++ Wk6-Strings-Grp1.py
@@ -5,12 +5,42 @@ Created on Fri Jun  3 08:27:52 2022
 @author: Marcus
 """
 
-a = 1
-b = 1.0
-c = "Tom"
-d = ["a","b", 1.0, 4]
 
-print("A is a", type(a))
-print("B is a", type(b))
-print("C is a", type(c))
-print("D is a", type(d))
+price = 3.2
+print(price)
+dollars = int(price)
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# a = 1
+# b = 1.0
+# c = "Tom"
+# d = ["a","b", 1.0, 4]
+# 
+# user = input("Give me a number: ")
+# print("The user gave me a", type(user))
+# user = float(user)
+# user = user//1
+# print("User is", user, type(user))
+# print("I have changed it to an integer =", int(user))
+# 
+# 
+# # answer = 4 * float(user)
+# # print("4 times", user,"is", answer)
+# # answer = 4 * user
+# # print("4 times", user,"is", answer)
+# # print("A is a", type(a))
+# # print("B is a", type(b))
+# # print("C is a", type(c))
+# # print("D is a", type(d))
+# 
+# 
+# =============================================================================
