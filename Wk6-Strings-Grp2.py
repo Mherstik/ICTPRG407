a8b07e64424f3f4b170e92029a5f2848b80507a9
Marcus Herstik <marcus@herstik.com>
2022-06-03 13:59:15 +1000

    Update Wk6-Strings-Grp2.py

--- Wk6-Strings-Grp2.py
+++ Wk6-Strings-Grp2.py
@@ -5,31 +5,81 @@ Created on Fri Jun  3 13:01:28 2022
 @author: Marcus
 """
 
-a = 1
-b = 12.7
-c = "Tom"
-
-d = str(23.9)
-
-print("A is a", type(a))
-print("B is a", type(b))
-print("C is a", type(c))
-
-print(a + b)
-print(b * 5) # float * 5
-print(c * 5) # string * 5
-
-print(str(b) + c)
-
-print("a + b = " + (str(a) + str(b)))
-
-print(int(b))
-print(float(a))
-
-print(b//2)
-print(b%2)
-print(len(c))
 
 
-#print(int(d))
+names = ["Tom Jones", "Barry White", "Urtha Kitt"]
 
+for name in names:
+    print(name)         
+
+# singer = "Tom Jones"
+# length = len(singer)
+# # print(length)
+# #print(singer[4])
+
+# #print(singer[9])
+# i = 0
+# while i < len(singer):
+#     print(i, singer[i])
+#     i = i + 1
+
+# print("######")
+
+# # for letter in singer:
+# #     print(letter)
+# for t in singer:
+#     print(t)
+
+
+
+
+# #singer[4] = 'p'
+# #### Lists
+
+# names = ["Tom Jones", "Barry White", "Urtha Kitt", 3.6, 4]
+# print(len(names))
+# print(names[2])
+
+# names[2] = 'p'
+# print(names[2])
+# print(type(names))
+# for name in names:
+#     print (name)
+
+
+
+# height = input("What is your height: ")
+# height = float(height)
+# print("You are " + str(height) + "m tall") 
+
+
+
+# a = 1
+# b = 12.7
+# c = "Tom"
+
+# d = str(23.9)
+# e = str(30)
+
+# print("A is a", type(a))
+# print("B is a", type(b))
+# print("C is a", type(c))
+
+# print(a + b)
+# print(b * 5) # float * 5
+# print(c * 5) # string * 5
+
+# print(str(b) + c)
+
+# print("a + b = " + (str(a) + str(b)))
+
+# print(int(b))
+# print(float(a))
+
+# print(b//2)
+# print(b%2)
+# print(len(c))
+
+
+# print(int(float(d)))
+# print(int(e))
