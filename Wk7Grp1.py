ff77d39ea2aeebd027add9c67f10bea5a8cf8619
Marcus Herstik <marcus@herstik.com>
2022-06-10 09:01:35 +1000

    Update Wk7Grp1.py

--- Wk7Grp1.py
+++ Wk7Grp1.py
@@ -8,10 +8,11 @@ Created on Fri Jun 10 08:22:29 2022
 ## Get file name
 
 fileName = input("What file do you want: ")
+# file2 = ""
 
 # Open the file
 fileInput = open(fileName, "r")
-
+# file2Input = open(filename,"r")
 
 ### Read lines from file
 for line in fileInput:
