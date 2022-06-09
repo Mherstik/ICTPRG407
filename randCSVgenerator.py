92183b8cd697654a04730dd42350ef04131aedd9
Marcus Herstik <marcus@herstik.com>
2022-06-10 09:51:52 +1000

    Update randCSVgenerator.py

--- randCSVgenerator.py
+++ randCSVgenerator.py
@@ -10,18 +10,18 @@ import random
 ## Generate random numbers
 i = 0
 fileName = "randomnum.txt"
-lines = int(input("How many lines: "))
+# lines = int(input("How many lines: "))
 
 ## open for writing: 
 outputFile = open(fileName, "w")
 
 ## get 10 random numbers into the file
-while i < lines:
+while i < 5:
     r1 = random.randint(0,50)
     r2 = random.randint(0,50)
     r3 = random.randint(0,10)
     outputFile.write(str(r1) + " , " + str(r2) + " , " + str(r3) +"\n")
-    i =+ 1
+    i = i + 1
 
 ## close the file
 outputFile.close()
