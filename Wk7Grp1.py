564c2f08a2902f09d737ac56a1b6848ed3c44714
Marcus Herstik <marcus@herstik.com>
2022-06-10 09:43:15 +1000

    New files

--- Wk7Grp1.py
+++ Wk7Grp1.py
@@ -5,18 +5,37 @@ Created on Fri Jun 10 08:22:29 2022
 @author: Marcus
 """
 
-## Get file name
+### Write to a file
 
-fileName = input("What file do you want: ")
-# file2 = ""
+fileName = input("What is the file name to create: ")
 
-# Open the file
-fileInput = open(fileName, "r")
-# file2Input = open(filename,"r")
+# open the file FOR WRITING!
+outputFile = open(fileName, "w")
+outputFile.write("This is NOT the first line\n")
+outputFile.write("This is the second line" + str(23) + "\n")
+outputFile.write("Line3\n")
 
-### Read lines from file
-for line in fileInput:
-    print(line, end='')
-    
-## Close the file
-fileInput.close()
+### Close the file
+
+outputFile.close()
+
+
+# =============================================================================
+# ### Read a file
+# 
+# ## Get file name
+# # fileName = input("What file do you want: ")
+# fileName = "Wk7Text.txt"
+# # file2 = ""
+# 
+# # Open the file
+# fileInput = open(fileName, "r")
+# # file2Input = open(filename,"r")
+# 
+# ### Read lines from file
+# for line in fileInput:
+#     print(line, end='')
+#     
+# ## Close the file
+# fileInput.close()
+# =============================================================================
---
+++ randCSVgenerator.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 09:28:46 2022
+
+@author: Marcus
+"""
+
+import random
+
+## Generate random numbers
+i = 0
+fileName = "randomnum.txt"
+lines = int(input("How many lines: "))
+
+## open for writing: 
+outputFile = open(fileName, "w")
+
+## get 10 random numbers into the file
+while i < lines:
+    r1 = random.randint(0,50)
+    r2 = random.randint(0,50)
+    r3 = random.random(0,10)
+    outputFile.write(str(r1) + "," + str(r2) + "," + str(r3) +"\n")
+    i =+ 1
+
+## close the file
+outputFile.close()
+
+
