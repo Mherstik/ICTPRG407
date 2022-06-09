204ce32e5d093fad3f714390e415df410b903e12
Marcus Herstik <marcus@herstik.com>
2022-06-10 08:43:05 +1000

    Update Wk7Grp1.py

--- Wk7Grp1.py
+++ Wk7Grp1.py
@@ -5,3 +5,17 @@ Created on Fri Jun 10 08:22:29 2022
 @author: Marcus
 """
 
+## Get file name
+
+fileName = input("What file do you want: ")
+
+# Open the file
+fileInput = open(fileName, "r")
+
+
+### Read lines from file
+for line in fileInput:
+    print(line, end='')
+    
+## Close the file
+fileInput.close()
